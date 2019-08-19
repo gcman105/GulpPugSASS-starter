1d32489f467b68f4803c053090b08c2dc0ba9cbd
gcman105 <gary@radi8.co.uk>
2019-08-19 17:02:41 +0200

    added bulma and pinegrow

---
+++ dest/_pgexport/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--  Converted from HTML to WordPress with Pinegrow Theme Converter for WordPress. https://pinegrow.com/theme-converter-for-wordpress  -->
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <meta content="Pinegrow Theme Converter for WordPress" name="generator">
+        <?php wp_head(); ?>
+    </head>
+    <body class="<?php echo implode(' ', get_body_class()); ?>">
+        <?php if( function_exists( 'wp_body_open' ) ) wp_body_open(); ?>
---
+++ dest/_pgexport/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<section class="section">
+    <div class="container">
+        <h1 class="title is-primary"><i class="far fa-angry"></i> <?php _e( 'Starter', 'gps_theme' ); ?><i class="fas fa-angry"></i></h1>
+        <h2 class="subtitle"><?php _e( 'by Gary Cheeseman', 'gps_theme' ); ?></h2>
+        <p><?php _e( 'This is some text also 6', 'gps_theme' ); ?></p>
+        <button class="button is-primary">
+            <?php _e( 'Button', 'gps_theme' ); ?>
+        </button>
+    </div>
+</section>        
+
+<?php get_footer(); ?>
